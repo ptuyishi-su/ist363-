308d47e6e9fd62379dc92e1427c7fcc56a9c31cc
Patrick Tuyishime <patricktuyishime@Patricks-MacBook-Air.local>
2023-09-03 14:29:27 -0400

    first commit

---
+++ Starbucks/js/main.js
@@ -0,0 +1,25 @@
+//console.log("js has been loaded");
+
+//variable, methods and functions
+
+//1. variables
+const myName= "Patrick Tuyishime"
+console.log(myName)
+
+const myAge=21
+const myCity="Syracuse"
+
+console.log(myAge,myCity)
+const introduction =`Hi, my name is ${myName}, I'm ${myAge}, and I'm from ${myCity}.`;
+console.log(introduction)
+
+//2. methods
+const menuBtn=document.getElementById("menuBtn");
+const overlay=document.getElementById("overlay")
+console.log(menuBtn)
+
+//"event name", callback function
+menuBtn.addEventListener("click", function(){
+  //console.log("menuBtn has been clicked")
+  overlay.classList.add("active")
+});//end of menuBtn click  event 
