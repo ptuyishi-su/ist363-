2e21ab817f478b551a814cd30e1719cdc47ecf31
Patrick Tuyishime <patricktuyishime@Patricks-MacBook-Air.local>
2023-09-19 18:23:02 -0400

    attendace 9/19/23

--- Starbucks/js/main.js
+++ Starbucks/js/main.js
@@ -1,61 +1,134 @@
-//console.log("js has been loaded");
+// console.log("js has been loaded");
 
-//variable, methods and functions
+// variables, methods and functions
 
-//1. variables
-const myName= "Patrick Tuyishime"
-console.log(myName)
+// 1. variables
+const myName = "Jeff Passetti";
+//console.log(myName);
 
-const myAge=21
-const myCity="Syracuse"
+const myAge = 21;
+const myCity = "Syracuse";
 
-console.log(myAge,myCity)
-const introduction =`Hi, my name is ${myName}, I'm ${myAge}, and I'm from ${myCity}.`;
-console.log(introduction)
+const introduction = `Hi, my name is ${myName}, I am ${myAge} years old and I live in ${myCity}.`;
 
-//2. methods
-const menuBtn=document.getElementById("menuBtn");
-const overlay=document.getElementById("overlay");
-const closeBtn =document.getElementById("closeBtn");
-//"event name", callback function
-menuBtn.addEventListener("click", function(){
-  //console.log("menuBtn has been clicked")
-  overlay.classList.add("active")
-});//end of menuBtn click  event 
-
-closeBtn.addEventListener("click", function(){
-  overlay.classList.remove("active");
-}); //end of closeBtn click event 
+//console.log(introduction);
 
 
-//array and objects
+// 2. methods
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeBtn");
+const overlay = document.getElementById("overlay");
+
+//console.log(menuBtn);
+
+// "event name", callback function
+menuBtn.addEventListener("click", function() {
+    //console.log("menu button has been clicked");
+    overlay.classList.add("active");
+}); // end of menuBtn click event
+
+closeBtn.addEventListener("click", function() {
+    overlay.classList.remove("active");
+}); // end of closeBtn click event
 
 
-//basic variable
-const cofeeName = "Mocha"
-const cofeeHeadline = document.createElement("h2");
-cofeeHeadline.textContent= cofeeName 
-document.body.appendChild(cofeeHeadline);
+// arrays and objects
 
+// basic variable
+const coffeeName = "Mocha";
+const coffeeHeadline = document.createElement("h2");
+coffeeHeadline.textContent = coffeeName;
+//document.body.appendChild(coffeeHeadline);
 
-//objectss
+// objects
 const coffee = {
-  name: "Mocha",
-  price: 3.99,
-  description: "This is Mocha Coffee"
+    name: "Mochaaaaaaa",
+    price: 3.99,
+    description: "This is a delicious cup of coffee."
 };
-
 //console.log(coffee.description);
 
-const coffeeOBJHeadline = document.createElement("h2")
-coffeeOBJHeadline.textContent= coffee.name;
-document.body.appendChild(coffeeOBJHeadline)
-
-const coffees= ["Mocha", "Latte", "espresso"];
-//console.log(coffees[2])
-
-coffees.forEach(function(coffee){
-  console.log(coffee)
-});
+const coffeeObjHeadline = document.createElement("h2");
+coffeeObjHeadline.textContent = coffee.name;
+//document.body.appendChild(coffeeObjHeadline);
 
 
+//basic array
+// const basicCoffees= ["mocha", "latte", "espresso"];
+// basicCoffees.forEach(function(basicCoffees){
+//   console.log(basicCoffees)
+
+// });//end of basicCoffees forEach method
+
+
+
+// const coffees = [
+//     {
+//         name: "Mocha",
+//         price: 3.99,
+//         description: "This is a delicious cup of coffee.",
+//         image: {
+//             fileName: "coffee.jpg",
+//             width: 630,
+//             height: 630,
+//             altText: "A cup of coffee."
+//         }
+//     },
+//     {
+//         name: "Latte",
+//         price: 3.99,
+//         description: "This is a delicious cup of coffee.",
+//         image: {
+//             fileName: "coffee.jpg",
+//             width: 630,
+//             height: 630,
+//             altText: "A cup of coffee."
+//         }
+//     },
+//     {
+//         name: "Espresso",
+//         price: 3.99,
+//         description: "This is a delicious cup of coffee.",
+//         image: {
+//             fileName: "coffee.jpg",
+//             width: 630,
+//             height: 630,
+//             altText: "A cup of coffee."
+//         }
+//     }
+// ];
+//console.log(coffees[2]);
+
+coffees.forEach(function(coffee) {
+    // 1 . deconstruct the coffee object
+    const { title, price, description, image } = coffee;
+
+    // 2. create the html elements
+    const coffeeArticle = document.createElement("article");
+    coffeeArticle.classList.add("coffee-items")
+
+    const coffeeImage = document.createElement("img");
+    coffeeImage.src = `images/${image.fileName}`;
+    coffeeImage.width = image.width;
+    coffeeImage.height = image.height;
+    coffeeImage.alt = image.altText;
+
+    const coffeeName = document.createElement("h2");
+    coffeeName.textContent = title;
+
+    const coffeePrice = document.createElement("h3");
+    coffeePrice.classList.add("coffee-price")
+    coffeePrice.textContent = `$${price}`;
+
+    const coffeeDescription = document.createElement("p");
+    coffeeDescription.textContent = description;
+
+    // 3. append the elements to the parent article
+    coffeeArticle.appendChild(coffeeImage);
+    coffeeArticle.appendChild(coffeeName);
+    coffeeArticle.appendChild(coffeePrice);
+    coffeeArticle.appendChild(coffeeDescription);
+
+    // 4. append the article to the body
+    document.body.appendChild(coffeeArticle);
+}); // end of coffees forEach method
